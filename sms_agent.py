9aecc7b3d05007a86eb72f76244c0355eb5ae0c2
tud7 <tduongcs@gmail.com>
2019-11-15 10:36:14 -0800

    use environment variable for authentication instead of config file

--- sms_agent.py
+++ sms_agent.py
@@ -1,47 +1,60 @@
 '''
 Author: Tu Duong
-Copyright © 2018 Tu Duong
+Copyright 2018, 2019 Tu Duong
 '''
 
 import os
 import json
+import argparse
 import sms_api_provider
 
 class SMSAgent(object):
 
+    SMS_API_PROVIDER = 'SMS_API_PROVIDER'
     TWILIO            = 'Twilio'
     SUPPORTED_SMS_API = [TWILIO]
 
-    def __init__(self, config_file):        
-        with open(config_file) as f:
-            self.config = json.load(f)
-
+    def __init__(self):
         self.sms_api_provider = self._sms_api_provider_factory()
 
-    
+
     def send_msg(self, phone_number, text_message):
         self.sms_api_provider.send_msg(phone_number, text_message)
-    
+
 
     def _sms_api_provider_factory(self):
-        
-        provider = self.config.get('SMS_API_Provider', None)
+
+        provider = os.getenv(SMSAgent.SMS_API_PROVIDER, None)
         if provider is None:
-            raise RuntimeError('Missing SMS API Provider information')
-        
+            raise RuntimeError('SMSAgent -- missing environment variable: %s' %SMSAgent.SMS_API_PROVIDER)
+
         if provider not in SMSAgent.SUPPORTED_SMS_API:
-            raise RuntimeError('Unsupported SMS API Provider (%s). Supported provider(s): %s' 
+            raise RuntimeError('Unsupported SMS API Provider (%s). Supported provider(s): %s'
                                 % (provider, ' '.join(SMSAgent.SUPPORTED_SMS_API)))
-        
-        auth   = self.config.get('Authentication')
-        
+
         module = __import__('sms_api_provider')
         klass  = getattr(module, provider)
-        return klass(auth)
+        return klass()
 
-'''
+
+def send_msg(to_number, message):
+    """
+    Call to send message to the provided to_number
+    Arguments:
+    to_number -- the string phone number of the person you want to send the message, start with +(country) (e.g.: "+12223334444")
+    nessage   -- the string text message that you want to send(e.g. " Hello World")
+    """
+    smsagent = SMSAgent();
+    smsagent.send_msg(to_number, message)
+
+
 if __name__== "__main__":
 
-    smsagent = SMSAgent('sms_api_provider.json');
-    smsagent.send_msg('+xxxxxxxxxx', 'Your message here!!!')
-'''
+    parser = argparse.ArgumentParser(description='SMS Agent')
+    parser.add_argument('to_number',          help='phone number of the person you want to send the message, start with +(country) (e.g.: +12223334444)')
+    parser.add_argument('message', nargs='+', help='the text message that you want to send in double quote (e.g. " Hello World")')
+
+    args = parser.parse_args()
+
+    message  =  ' '.join(args.message) # solution to parse string with spaces from command line
+    send_msg(args.to_number, message)
--- sms_api_provider.py
+++ sms_api_provider.py
@@ -1,6 +1,6 @@
 '''
 Author: Tu Duong
-Copyright © 2018 Tu Duong
+Copyright 2018, 2019 Tu Duong
 '''
 
 import os
@@ -10,53 +10,49 @@ from twilio.rest import Client
 
 class SMSAPIProvider(object):
 
-    def __init__(self, auth_dict):
-        if auth_dict is None:
-            raise RuntimeError('error: missing authentication information for Twilio')
-            
-        self.authentication_dict = auth_dict
+    ACCOUNT_ID          = 'ACCOUNT_ID'
+    AUTH_TOKEN          = 'AUTH_TOKEN'
+    CALLER_PHONE_NUMBER = 'CALLER_PHONE_NUMBER'
 
-    
+    def __init__(self):
+
+        _missing_env = []
+
+        self.account_id          = os.getenv(SMSAPIProvider.ACCOUNT_ID, None)
+        self.auth_token          = os.getenv(SMSAPIProvider.AUTH_TOKEN, None)
+        self.caller_phone_number = os.getenv(SMSAPIProvider.CALLER_PHONE_NUMBER, None)
+
+        # validate the authentication
+        if not self.account_id:
+            _missing_env.append(SMSAPIProvider.ACCOUNT_ID)
+        if not self.auth_token:
+            _missing_env.append(SMSAPIProvider.AUTH_TOKEN)
+        if not self.caller_phone_number:
+            _missing_env.append(SMSAPIProvider.CALLER_PHONE_NUMBER)
+
+        if _missing_env:
+            raise RuntimeError('SMSAPIProvider -- missing environment variable(s):  %s' %_missing_env)
+
     @property
     def send_msg(self, phone_number, text_message):
         raise NotImplementedError('error: subclasses should implement this!')
 
-        
-class Twilio(SMSAPIProvider):
-     
-    ACCOUNT_ID   = 'account_id'
-    AUTH_TOKEN   = 'auth_token'
-    FROM_NUMBER  = 'from_number'
 
-    def __init__(self, auth_dict):
-        
-        SMSAPIProvider.__init__(self, auth_dict)
-        
-        # get Twilio authentication from configuration file
-        self.account_id      = self.authentication_dict.get(Twilio.ACCOUNT_ID,   None)
-        self.auth_token      = self.authentication_dict.get(Twilio.AUTH_TOKEN,   None)
-        self.my_phone_number = self.authentication_dict.get(Twilio.FROM_NUMBER,  None)
-        
-        # validate the authentication
-        if not self.account_id:
-            raise RuntimeError('Twilio --missing %s') % Twilio.ACCOUNT_ID
-        if not self.auth_token:
-            raise RuntimeError('Twilio --missing %s') % Twilio.AUTH_TOKEN
-        if not self.my_phone_number:
-            raise RuntimeError('Twilio --missing %s') % Twilio.FROM_NUMBER
-        
+class Twilio(SMSAPIProvider):
+
+    def __init__(self):
+
+        SMSAPIProvider.__init__(self)
+
         # create Twilio client
         self.client  = Client(self.account_id, self.auth_token)
-    
-    
+
+
     def send_msg(self, phone_number, text_message):
-    
+
         if not phone_number:
-            raise RuntimeError('Twilio --missing to phone number')
+            raise RuntimeError('Twilio --invalid phone number')
         if not text_message:
-            raise RuntimeError('Twilio --missing text message')
-            
-        message = self.client.messages.create(to=phone_number, from_=self.my_phone_number, body=text_message)
+            raise RuntimeError('Twilio --invalid text message')
 
-
-
+        message = self.client.messages.create(to=phone_number, from_=self.caller_phone_number, body=text_message)
